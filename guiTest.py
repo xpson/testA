fc89f267ec00ffb0f3a57b751fe7d42d64e04240
xpson <sunxpeng@gmail.com>
2016-05-20 17:32:54 +0800

    512

--- guiTest.py
+++ guiTest.py
@@ -29,7 +29,7 @@ hbox.Add(saveButton,proportion=0,flag=wx.LEFT,border=5)
 
 vbox=wx.BoxSizer(wx.VERTICAL)
 vbox.Add(hbox,proportion=0,flag=wx.EXPAND|wx.ALL,border=5)
-vbox.Add(contents,proportion=0,flag=wx.EXPAND | wx.LEFT|wx.BOTTOM | wx.RIGHT,border=5)
+vbox.Add(contents,proportion=1,flag=wx.EXPAND | wx.LEFT|wx.BOTTOM | wx.RIGHT,border=5)
 bkg.SetSizer(vbox)
 win.Show()
 
