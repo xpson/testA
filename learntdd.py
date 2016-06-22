bb3144c52a384d81d8f938ffd3d175227e3013e2
xpson <sunxiaopeng@nongfadai.com>
2016-06-22 19:44:36 +0800

    test

--- learntdd.py
+++ learntdd.py
@@ -1,4 +1,11 @@
-from selenium import webdriver
-browser=webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+#coding=utf-8
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1>我是你的妈妈!</h1>'
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>hello, %s!</h1>'%name
+if __name__ =='__main__':
+    app.run(debug=True)
